842146e706af68250b7c763196f05ccee3d0f8d8
HammadNadeem21 <nhammad424@gmail.com>
2025-06-07 23:17:40 +0500

    Agent Compeleted

--- Ai-Agents/Openai-Agents-SDK/main.py
+++ Ai-Agents/Openai-Agents-SDK/main.py
@@ -14,11 +14,14 @@ provider = AsyncOpenAI(
 )
 
 # Configure the language model
-model = OpenAIChatCompletionsModel(model="gemini-2.0-flash", openai_client=provider)
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=provider
+)
 
 greeting_agent = Agent(
     name="Greeting Agent",
-    instructions="You are a greeting agent, your task is to say Waalaiku-Alssalam, when someone says Assalamualaikum or anything similar.",
+    instructions="You are a greeting agent, your task is to say Waalaiku-Alssalam, when someone says Assalamualaikum or anything similar, if someone says bye than say Allah Hafiz from Hammad, when someone asks other than greeting then say I'm a Greeting Agent, if someone says who was the founder of the pakistan then say Quaid-e-Azam Muhammad Ali Jinah.",
     model=model,
     
 )
@@ -27,6 +30,6 @@ greeting_agent = Agent(
 user_input = input("Enter a message: ")
 
 
-agent_result = Runner.run_sync(greeting_agent, user_input)
+agent_result = Runner.run_sync(greeting_agent, user_input )
 
 print(agent_result.final_output)
